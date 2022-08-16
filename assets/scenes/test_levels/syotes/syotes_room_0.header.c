77724d39c258a6ff4f940ae7b18070fc96a0d0b8
ArcticLuma113 <aplumafreak500@gmail.com>
2022-08-15 21:09:16 -0400

    Room 120 fix (preliminary)

---
+++ assets/scenes/test_levels/syotes/syotes_room_0.header.c
@@ -0,0 +1,29 @@
+#include "ultra64.h"
+#include "z64.h"
+#include "macros.h"
+#include "syotes_room_0.h"
+#include "segment_symbols.h"
+#include "command_macros_base.h"
+#include "z64cutscene_commands.h"
+#include "variables.h"
+#include "syotes_scene.h"
+
+s16 syotes_room_0ObjectList[];
+ActorEntry syotes_room_0ActorList[];
+
+SceneCmd syotes2_room_0Commands[] = {
+    SCENE_CMD_ECHO_SETTINGS(5),
+    SCENE_CMD_ROOM_BEHAVIOR(0x01, 0x00, false, false),
+    SCENE_CMD_SKYBOX_DISABLES(true, false),
+    SCENE_CMD_TIME_SETTINGS(0, 0, 255),
+    SCENE_CMD_MESH(&syotes_room_0PolygonType0_000000),
+    SCENE_CMD_OBJECT_LIST(1, syotes_room_0ObjectList),
+    SCENE_CMD_ACTOR_LIST(1, syotes_room_0ActorList),
+    SCENE_CMD_END(),
+};
+
+s16 syotes_room_0ObjectList[] = {
+};
+
+ActorEntry syotes_room_0ActorList[] = {
+};
